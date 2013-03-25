819e6a4be5fd68c2fd63764496484881804e864d
vorce <joel@vorce.se>
2013-03-25 19:33:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Contest
+
+** TODO: Add description **
---
+++ test/contest_test.exs
@@ -0,0 +1,9 @@
+Code.require_file "../test_helper.exs", __FILE__
+
+defmodule ContestTest do
+  use ExUnit.Case, async: true
+
+  test "the truth" do
+    assert 1 + 1 == 3
+  end
+end
